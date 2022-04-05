3ccd43e41d1a7dd3f0a56e8a8560ed107ff46120
Anthony Attard <anthony@anthonyattard.com>
2022-04-05 05:55:05 +0000

    Clean up

--- src/extension.ts
+++ src/extension.ts
@@ -1,24 +1,11 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "zoomer" is now active!');
+  console.log("Zoomer Activated");
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
   let disposable = vscode.commands.registerCommand("zoomer.helloWorld", () => {
-    // The code you place here will be executed every time your command is executed
-
-    // Display a message box to the user
     vscode.window.showInformationMessage("Hello from zoomer!");
   });
-
 
   let zoomIns = vscode.commands.registerCommand("zoomer.zoomIns", () => {
     // vscode.window.showInformationMessage("zoomIns");
@@ -34,12 +21,10 @@ export function activate(context: vscode.ExtensionContext) {
     vscode.workspace
       .getConfiguration("")
       .update("window.zoomLevel", parseFloat(total.toFixed(2)), true);
-
   });
 
   const zoomOuts = vscode.commands.registerCommand("zoomer.zoomOuts", () => {
     // vscode.window.showInformationMessage("zoomOuts");
-
     const customLevel: any = vscode.workspace
       .getConfiguration("")
       .get("zoomer.zoomIncrement");
@@ -57,5 +42,4 @@ export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(zoomOuts);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
